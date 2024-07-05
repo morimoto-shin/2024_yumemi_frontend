d2b3c39ba7859a9ee897867bca3ee3070b1cc926
森元心 <shinshinshin0914@gmail.com>
2024-07-05 23:48:24 +0900

    add: install test tools

---
+++ app/vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineVitestConfig } from "@nuxt/test-utils/config";
+
+export default defineVitestConfig({
+  test: {
+    environmentOptions: {
+      nuxt: {
+        mock: {
+          intersectionObserver: true,
+        },
+      },
+    },
+  },
+});
